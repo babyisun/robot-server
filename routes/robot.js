6f5bec8b5348d69ca45c68a0cdde6605b9138deb
North <YangShi2@sf-express.com>
2019-07-10 22:40:54 +0800

    send msg

--- routes/robot.js
+++ routes/robot.js
@@ -9,24 +9,28 @@ const {
 // 发送消息
 router.post('/', function (req, res, next) {
     var {
+        url,
         key,
         user
     } = req.body;
-    // var post_data = {
-    //     "msgtype": "text",
-    //     "text": {
-    //         "content": "发送测试：" + moment().format('YYYY-MM-DD HH:mm:ss'),
-    //         // "mentioned_list": ["wangqing", "@all"],
-    //     }
-    // };
-    res.json(json({
-        key,
-        user,
-        a: 123
-    }));
-    // ajax.post("/cgi-bin/webhook/send?key=7ced3ef6-aea1-48de-a49b-817bc2a44bad", post_data).then(function (data) {
-    //     res.json(data);
-    // }).catch(next);
+    var post_data = {
+        "msgtype": "markdown",
+        "markdown": {
+            "content": `Bug反馈<font color=\"warning\">132个</font>，请相关同事注意。\n
+>自测:<font color=\"info\">1例</font> \n >QA反馈:<font color=\"comment\">15例</font> \n
+[本消息来自小机机](http://127.0.0.1:3000)`,
+            // "content": "发送测试：" + moment().format('YYYY-MM-DD HH:mm:ss'),
+            // "mentioned_list": ["wangqing", "@all"],
+        }
+    };
+    // res.json(json({
+    //     key,
+    //     user,
+    //     a: 123
+    // }));
+    ajax.post(url, post_data).then((data) => {
+        res.json(json(data));
+    }).catch(next);
 
 });
 
